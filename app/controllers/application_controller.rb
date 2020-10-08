6ad0c93acd89cd14b09e009e1ee29a4692024776
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-10-08 11:18:15 +0900

    ユーザー管理機能の実装
    ログイン、ログアウト
    正規表現、エラーメッセージ表示
    config/application.rbに
    一文を追加しviewの崩れ防止

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_first])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_last])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name_reading_first])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name_reading_last])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,35 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-         validates :nickname, :first_name, :last_name, :name_reading_first, :name_reading_last, :birtday, presence: true
-         validates :nickname, presence: { message: "Nickname can't be blank" }
-         validates :email, presence: { message: "Email can't be blank" }
-         validates :password, presence: { message: "Password can't be blank" }
-         validates :first_name, presence: { message: "First name can't be blank" }
-         validates :last_name, presence: { message: "Last name can't be blank" }
-         validates :name_reading_first, presence: { message: "First name kana can't be blank" }
-         validates :name_reading_last, presence: { message: "Last name kana can't be blank" }
-         validates :birthday, presence: { message: "Birth date can't be blank" }
+        #  validates :nickname, :name_first, :name_last, :name_reading_first, :name_reading_last, :birthday, presence: true
+        #  validates :nickname, presence: { message: "Nickname can't be blank" }
+        #  validates :nickname, presence: true, { message: "Nickname can't be blank" }
+        #  validates :email, presence: { message: "Email can't be blank" }
+        #  validates :password, presence: { message: "Password can't be blank" }
+        #  validates :name_first, presence: { message: "First name can't be blank" }
+        #  validates :name_last, presence: { message: "Last name can't be blank" }
+        #  validates :name_reading_first, presence: { message: "First name kana can't be blank" }
+        #  validates :name_reading_last, presence: { message: "Last name kana can't be blank" }
+        #  validates :birthday, presence: { message: "Birth date can't be blank" }
 
-         validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
-         validates :password, confirmation: { message: Password Include both letters and numbers}
-         validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "First name Full-width characters"}
-         validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Last name Full-width characters"}
-         validates :name_reading_first, format: { with: /\A[ァ-ヶー－]+\z/, message: "First name kana Full-width katakana characters"}
-         validates :name_reading_last, format: { with: /\A[ァ-ヶー－]+\z/, message: "Last name kana Full-width katakana characters"}
+        #  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
+        #  validates :password, confirmation: { message: "Password Include both letters and numbers" }
+        #  validates :name_first, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "First name Full-width characters"}
+        #  validates :name_last, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Last name Full-width characters"}
+        #  validates :name_reading_first, format: { with: /\A[ァ-ヶー－]+\z/, message: "First name kana Full-width katakana characters"}
+        #  validates :name_reading_last, format: { with: /\A[ァ-ヶー－]+\z/, message: "Last name kana Full-width katakana characters"}
+        
+        # validates :nickname, :birthday, presence: true
 
+        with_options presence: true do
+          validates :nickname
+          validates :birthday
+          validates :name_first, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+          validates :name_last, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+          validates :name_reading_first, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+          validates :name_reading_last, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+        end
+        #validates :password, format: { with: /\A[a-z0-9]+\z/i }
+        validates :password, format: { with: /(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]/, message: "Include both letters and numbers" }
+        #validates :password_confirmation, confirmation: { message: "Password Include both letters and numbers" }
 end
