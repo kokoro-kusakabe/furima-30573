9a889da5e947b1b9ae8d73045ea8abe37e8146ae
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-10-09 15:36:26 +0900

    ルーティングとコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    @item =Item.new
+  end
+
+  def create
+      @item =Item.new(item_prams)
+    if @item.save
+      redirect_to root_path
+    else
+      @item =Item.new(item_prams)
+      render :new
+    end
+  end
+
+  private
+  def items_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :burden_id, :area_id, :day_id).merge(user_id: current_user.id)
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category, :status, :burden, :area, :day
 
-  validates :, :name, :description, :category, :status, :burden, :area, :day, presence: true
+  validates :name, :description, :category, :status, :burden, :area, :day, presence: true
 
   validates :category_id, :status_id, :burden_id, :area_id, :day_id, numericality: { other_than: 0 } 
 end
