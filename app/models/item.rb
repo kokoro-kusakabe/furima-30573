6b355d398c105fa1136212a49b17e4bba7e31c9b
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-10-11 19:36:22 +0900

    itemモデルをwith optionsでまとめる

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,14 +6,17 @@ class Item < ApplicationRecord
   belongs_to_active_hash :area
   belongs_to_active_hash :day
 
-  validates :image, :name, :description, :category, :status, :burden, :area, :day, :price, presence: true
-
-  validates :category_id, numericality: { other_than: 0 , message: "Select" }
-  validates :status_id, numericality: { other_than: 0 , message: "Select" }
-  validates :burden_id, numericality: { other_than: 0 , message: "Select" }
-  validates :area_id, numericality: { other_than: 0 , message: "Select" }
-  validates :day_id, numericality: { other_than: 0 , message: "Select" }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id, numericality: { other_than: 0 , message: "Select" }
+    validates :status_id, numericality: { other_than: 0 , message: "Select" }
+    validates :burden_id, numericality: { other_than: 0 , message: "Select" }
+    validates :area_id, numericality: { other_than: 0 , message: "Select" }
+    validates :day_id, numericality: { other_than: 0 , message: "Select" }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+  end
   validates :price, format: { with: /\A[0-9]+\z/ , message: "Half-width number"}
 
   has_one_attached :image
