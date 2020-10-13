f7ae15b042898ab06192d28ea6ebdf347f83c916
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-10-13 18:19:05 +0900

    ルーティングとコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,6 +37,12 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :burden_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
