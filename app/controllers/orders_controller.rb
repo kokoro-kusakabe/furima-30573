d2c00ca67636997ab7c78438f81632d56722edf1
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-10-14 15:54:10 +0900

    controllerデータ保存の流れを記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,28 @@
 class OrdersController < ApplicationController
 
   def index
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    @address = Address.new(address_params(order))
+    if @order.valid?
+      @order.save
+      @address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
-  
+
+  private
+  def order_params
+    params.require(:order).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def address_params(order)
+    params.permit(:postal_code, :area_id, :city, :house_numbe, :phone_number).merge(order_id: order.id)
+  end
+
 end
