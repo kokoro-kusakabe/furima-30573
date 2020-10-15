27a8b5be2bc69f590bf77e1ed0117e0d8a08834b
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-10-15 11:34:36 +0900

    controllerの受け取る情報設定(仮)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,10 +7,10 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
-    @address = Address.new(address_params(order))
-    if @order.valid?
-      @order.save
-      @address.save
+    @address = Address.new(address_params(@order))
+    if 
+        @order.save
+        @address.save
       redirect_to root_path
     else
       render :index
@@ -19,11 +19,11 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order).permit(:token).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def address_params(order)
-    params.permit(:postal_code, :area_id, :city, :house_numbe, :phone_number).merge(order_id: order.id)
+    params.permit(:postal_code, :area_id, :city, :house_number, :phone_number).merge(order_id: order.id)
   end
 
 end
