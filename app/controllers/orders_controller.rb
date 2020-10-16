9c4d19500b3a94694a03a0e1b0ab369c99754f36
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-10-16 14:52:42 +0900

    Formオブジェクトを用いてorder/addressモデルを１つした

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,47 @@
 class OrdersController < ApplicationController
 
+  # def index
+  #   @order = Order.new
+  #   @item = Item.find(params[:item_id])
+  # end
+
+  # def create
+  #   @item = Item.find(params[:item_id])
+  #   @order = Order.new(order_params)
+  #   @address = Address.new(address_params(@order))
+  #   if  @order.valid?
+  #       pay_item
+  #       @order.save
+  #       @address.save
+  #     redirect_to root_path
+  #   else
+  #     render :index
+  #   end
+  # end
+
+  # private
+  # def order_params
+  #   params.require(:order).permit().merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  # end
+
+  # def address_params(order)
+  #   params.permit(:postal_code, :area_id, :city, :house_number, :phone_number).merge(order_id: order.id)
+  # end
+
+  
+  # before_action :set_order, only: [:index, :crate]
+
   def index
-    @order = Order.new
     @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new
   end
 
   def create
     @item = Item.find(params[:item_id])
-    @order = Order.new(order_params)
-    @address = Address.new(address_params(@order))
-    if  @order.valid?
-        pay_item
-        @order.save
-        @address.save
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
       redirect_to root_path
     else
       render :index
@@ -21,12 +50,13 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit().merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_form).permit(:postal_code, :area_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
-  def address_params(order)
-    params.permit(:postal_code, :area_id, :city, :house_number, :phone_number).merge(order_id: order.id)
-  end
+
+  # def set_order
+  #   @item = Item.find(params[:item_id])
+  # end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -8,15 +8,17 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("order[number]"),
-      cvc: formData.get("order[cvc]"),
-      exp_month: formData.get("order[exp_month]"),
-      exp_year: `20${formData.get("order[exp_year]")}`,
+      number: formData.get("order_form[number]"),
+      cvc: formData.get("order_form[cvc]"),
+      exp_month: formData.get("order_form[exp_month]"),
+      exp_year: `20${formData.get("order_form[exp_year]")}`,
     };
 
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
+        // console.log(token)
+        // debugger
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden"> `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
