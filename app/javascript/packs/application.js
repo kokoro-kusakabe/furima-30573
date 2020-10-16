74c214b47339a9e2ec279d96722bd1d40a54a9af
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-10-16 18:08:10 +0900

    soldoutの表示

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("@rails/activestorage").start()
 require("channels")
 require("../calculation")
 require("../card")
-require("../sold_out")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
