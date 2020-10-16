ef313a484b6374b1f33b0fb9d69d3d772a4830b2
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-10-16 16:56:51 +0900

    ユーザー,商品の状態あを判別し適切な画面遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,43 +1,19 @@
 class OrdersController < ApplicationController
 
-  # def index
-  #   @order = Order.new
-  #   @item = Item.find(params[:item_id])
-  # end
-
-  # def create
-  #   @item = Item.find(params[:item_id])
-  #   @order = Order.new(order_params)
-  #   @address = Address.new(address_params(@order))
-  #   if  @order.valid?
-  #       pay_item
-  #       @order.save
-  #       @address.save
-  #     redirect_to root_path
-  #   else
-  #     render :index
-  #   end
-  # end
-
-  # private
-  # def order_params
-  #   params.require(:order).permit().merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
-  # end
-
-  # def address_params(order)
-  #   params.permit(:postal_code, :area_id, :city, :house_number, :phone_number).merge(order_id: order.id)
-  # end
-
-  
-  # before_action :set_order, only: [:index, :crate]
+  before_action :authenticate_user!
+  before_action :set_order, only: [:index, :crate]
 
   def index
-    @item = Item.find(params[:item_id])
-    @order_form = OrderForm.new
+      if  @item.order.present? 
+          redirect_to root_path
+      elsif  @item.user.id == current_user.id
+          redirect_to root_path
+      else
+          render :index
+      end
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
       pay_item
@@ -53,10 +29,10 @@ class OrdersController < ApplicationController
     params.require(:order_form).permit(:postal_code, :area_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
-
-  # def set_order
-  #   @item = Item.find(params[:item_id])
-  # end
+  def set_order
+    @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new
+  end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
